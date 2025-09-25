d147b1483edf9a0353228945916304b022b83523
Mukesh Vaghashiya <mukeshvaghashiya@Mukeshs-MacBook-Pro.local>
2025-09-25 16:09:29 -0500

    Week 4 Day 2 Homework

--- js/randomcodes.js
+++ js/randomcodes.js
@@ -1,16 +1,42 @@
+
+var generatedCode = '';
+
 function generateCode() {
     var code = '';
     var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     for (var i = 1; i <= 8; i++) {
-        var char = Math.random() * str.length;
-        code += str.charAt(char)
+        var char = Math.floor(Math.random() * str.length);
+        code += str.charAt(char);
     }
+    generatedCode = code;
     return code;
 }
-document.getElementById("codes").innerHTML = generateCode();
 
-function disableButton(){
-    document.getElementById("submit").disabled = true;
+function disableButton(btnvalue) {
+    var submitBtn = document.getElementById("submit");
+    submitBtn.disabled = btnvalue;
+    if (btnvalue) {
+        submitBtn.style.backgroundColor = "rgba(73, 119, 209, 0.3)";
+        submitBtn.style.color = "rgba(255, 255, 255, 0.5)";
+    } else {
+        submitBtn.style.backgroundColor = "rgba(73, 119, 209, 1)";
+        submitBtn.style.color = "rgba(255, 255, 255, 1)";
+    }
 }
 
-disableButton();
+function evaluateCode() {
+    var getCode = document.getElementById("codeentered").value.trim();
+    var code = generatedCode.trim();
+    if (getCode.length === code.length && getCode === code) {
+        disableButton(false);
+    } else {
+        disableButton(true);
+    }
+}
+
+window.onload = function() {
+    document.getElementById("codes").innerHTML = generateCode();
+    disableButton(true);
+    var codebox = document.getElementById("codeentered");
+    codebox.addEventListener("input", evaluateCode);
+};
