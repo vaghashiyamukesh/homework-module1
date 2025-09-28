0aae12cec21d828811311361a3c5005ed91694fc
Mukesh Vaghashiya <mukeshvaghashiya@Mukeshs-MacBook-Pro.local>
2025-09-27 19:08:51 -0500

    Week 5 Day 3 Homework

--- js/activity.js
+++ js/activity.js
@@ -1,12 +1,24 @@
 $(document).ready(function() {
-    $('table tbody td:not(:first-child)').each(function() {
-        if ($(this).text().trim() !== "Not Available") {
-            $(this).addClass('selectable');
-        }
-    });
+    $("td").click(function () { //user select a table data cell
+        var content = $(this).text(); //get content of cell
+        var cellIndex = $(this).index(); //get the index of clicked cell
+        var cliffSite = $("thead th").eq(cellIndex).text(); //get cliff site name from header
+        
+        if (content != "Not Available") { //check if content does not contain a particular string
+            $(this).toggleClass("selectable"); //add or remove class when cell is selected
 
-    $('.selectable').click(function() {
-        // Toggle the selected class on click
-        $(this).toggleClass('selected');
+            if ($(this).hasClass("selectable")) { //check if selected cell has class
+                $('#displaySelected').css("visibility","visible"); //make display box visible
+                $('#displaySelected').css("margin-top","2em"); //add spaces above display box
+                $('#result').append("<p>" + content + " at " + cliffSite + "</p><br>"); //add activity and location
+            } else { //if selected cell don't have class
+                $('#result p:contains('+content+')').remove(); //remove child element
+                
+                if ($('#result').has('p').length == false) { //check if there are any child elements within parent
+                    $('#displaySelected').css("visibility","hidden"); //make display box hidden
+                    $('#displaySelected').css("margin-top","0"); //remove spaces above display box
+                }
+            }
+        }
     });
 });
